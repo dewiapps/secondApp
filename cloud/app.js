8c4259d7ed93f22650dfbf2071075d8e531d9f2b
dewiapps <natedeanmail@gmail.com>
2014-09-03 15:21:04 -0500

    Making this commit from a PC.  So, thats pretty cool

--- cloud/app.js
+++ cloud/app.js
@@ -11,7 +11,7 @@ app.use(express.bodyParser());    // Middleware for reading request body
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Seriously Nate, this is the hello page!' });
+  res.render('hello', { message: 'This page was created by Nates PC!  Using Git and Parse!' });
 });
 
 // // Example reading from the request query string of an HTTP get request.
