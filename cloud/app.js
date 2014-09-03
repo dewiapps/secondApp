cf15d5990bd8d9cd2eb7a9aad015e017707bd89d
dewiapps <natedeanmail@gmail.com>
2014-09-03 17:00:45 -0500

    Added more stuff

--- cloud/app.js
+++ cloud/app.js
@@ -1,12 +1,15 @@
 
-// These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
+var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+var parseExpressCookieSession = require('parse-express-cookie-session');
 var app = express();
 
-// Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+app.set('views', 'cloud/views');
+app.set('view engine', 'ejs');
+app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
+app.use(express.bodyParser());
+app.use(express.cookieParser('YOUR_SIGNING_SECRET'));
+app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
@@ -17,6 +20,77 @@ app.get('/hello', function(req, res) {
 app.get('/', function(req, res) {
   res.render('home', { message: 'This home page was created by Nates PC!  Using Git and Parse!' });
 });
+
+// You could have a "Log In" link on your website pointing to this.
+app.get('/login', function(req, res) {
+  // Renders the login form asking for username and password.
+  res.render('login.ejs');
+});
+
+// Clicking submit on the login form triggers this.
+app.post('/login', function(req, res) {
+  Parse.User.logIn(req.body.username, req.body.password).then(function() {
+    // Login succeeded, redirect to homepage.
+    // parseExpressCookieSession will automatically set cookie.
+    res.redirect('/');
+  },
+  function(error) {
+    // Login failed, redirect back to login form.
+    res.render('login', {flash: error.message});
+  });
+});
+
+// You could have a "Log Out" link on your website pointing to this.
+app.get('/logout', function(req, res) {
+  Parse.User.logOut();
+  res.redirect('/');
+});
+
+// The homepage renders differently depending on whether user is logged in.
+app.get('/', function(req, res) {
+  if (Parse.User.current()) {
+    // No need to fetch the current user for querying Note objects.
+    var Note = Parse.Object.extend("Note");
+    var query = new Parse.Query(Note);
+    query.find().then(function(results) {
+      // Render the notes that the current user is allowed to see.
+    },
+    function(error) {
+      // Render error page.
+    });
+  } else {
+    // Render a public welcome page, with a link to the '/login' endpoint.
+  }
+});
+
+// You could have a "Profile" link on your website pointing to this.
+app.get('/profile', function(req, res) {
+  // Display the user profile if user is logged in.
+  if (Parse.User.current()) {
+    // We need to fetch because we need to show fields on the user object.
+    Parse.User.current().fetch().then(function(user) {
+      // Render the user profile information (e.g. email, phone, etc).
+    },
+    function(error) {
+      // Render error page.
+    });
+  } else {
+    // User not logged in, redirect to login form.
+    res.redirect('/login');
+  }
+});
+
+app.post('/partsInput', function(req, res) {
+    var newPart = new Parse.Object("Parts");
+    newPart.set("name", req.body.part);
+    newPart.save().then(function(){
+      response.success(req.body.part + ' saved as new part.');
+    });
+  },
+  function(error) {
+    response.error("Something went wrong.  Part not saved.");
+  });
+
 
 
 
